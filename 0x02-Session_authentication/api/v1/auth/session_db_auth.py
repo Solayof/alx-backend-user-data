f02ebd981125254de09630870bdc366e507b1240
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-07-11 22:29:59 +0100

    advance task

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,6 +4,7 @@ API session db module
 """
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
 from os import getenv
 
 
@@ -12,7 +13,9 @@ class SessionDBAuth(SessionExpAuth):
 
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for user_id """
-        pass
+        session_id = super().create_session(user_id)
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Returns User ID based on Session ID """
@@ -20,7 +23,8 @@ class SessionDBAuth(SessionExpAuth):
         if session_id is None or isinstance(session_id, str) is False:
             return None
         else:
-            pass
+            user = UserSession.get(session_id=session_id)
+            return user.user_id
 
     def destroy_session(self, request=None):
         """ Deletes user session to logout """
